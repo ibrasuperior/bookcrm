cf4c71419ea9a39896e32b32b8bace8773ef7029
ibrasuperior <47188758+ibrasuperior@users.noreply.github.com>
2019-08-01 13:33:47 -0300

    Alter search page

--- resources/views/leads/search.blade.php
+++ resources/views/leads/search.blade.php
@@ -3,22 +3,47 @@
 @section('content')
 
 <div class="container-fluid">
-<h1 class="ls-title-intro ls-ico-users">Resultado da Pesquisa</h1>
+      <h1 class="ls-title-intro ls-ico-users">Pesquisa</h1>
+
+@if( session('success') ) 
+  <div class="ls-alert-success ls-dismissable">
+    <span data-ls-module="dismiss" class="ls-dismiss">&times;</span>
+    {{ session('success') }}
+  </div>
+@endif
+
+@if( session('danger') ) 
+  <div class="ls-alert-danger ls-dismissable">
+    <span data-ls-module="dismiss" class="ls-dismiss">&times;</span>
+    {{ session('danger') }}
+  </div>
+@endif
+
+<style>
+.shadow:hover{
+border-radius: 5px;
+-webkit-box-shadow: 0px 1px 3px 1px rgba(0,0,0,0.45);
+-moz-box-shadow: 0px 1px 3px 1px rgba(0,0,0,0.45);
+box-shadow: 0px 1px 3px 1px rgba(0,0,0,0.15);
+}
+
+</style>
 
 <div class="ls-box-filter" style="-webkit-box-shadow: 0px 1px 3px 1px rgba(0,0,0,0.45);
 -moz-box-shadow: 0px 1px 3px 1px rgba(0,0,0,0.45);
 box-shadow: 0px 1px 3px 1px rgba(0,0,0,0.15);">
-  <form action="" class="ls-form ls-form-inline ls-float-left">
+  <form action="/leads/filter" class="ls-form ls-form-inline ls-float-left">
     <label class="ls-label col-md-6 col-sm-8">
       <b class="ls-label-text">Status</b>
       <div class="ls-custom-select ls-field-sm">
-        <select name="" class="ls-select">
+        <select name="order" class="ls-select">
           <option>Todos</option>
-          <option>Matriculados</option>
-          <option>Defeituosos</option>
+          <option value="2" >Oportunidades </option>
+          <option value="4" >Desqualificados </option>
         </select>
       </div>
     </label>
+    <button class="submit ls-btn-primary">Filtrar</button>
   </form>
 
   <form action="/leads/search" method="GET" class="ls-form ls-form-inline ls-float-right">
@@ -31,26 +56,26 @@ box-shadow: 0px 1px 3px 1px rgba(0,0,0,0.15);">
   </form>
 </div>
 
-<table class="ls-table">
+<table class="ls-table  ls-bg-header ">
   <thead>
     <tr>
-      <th class="ls-txt-center" >Lead</th>
-      <th class="ls-txt-center">E-mail</th>
-      <th class="ls-txt-center">Telefone</th>
-      <th class="ls-txt-center">Mídia</th>
-      <th class="ls-txt-center">Colaborador</th>
-      <th class="ls-txt-center">Ações</th>
-      
-      
+      <th class="ls-txt-center ls-data-descending" > <a> Lead</a> </th>
+      <th class="ls-txt-center ls-data-descending"> <a>E-mail </a></th>
+      <th class="ls-txt-center ls-data-descending"><a>Telefone </a></th>
+      <th class="ls-txt-center ls-data-descending"><a>Mídia </a></th>
+      <th class="ls-txt-center ls-data-descending"><a>Ações </a></th>
     </tr>
   </thead>
   <tbody>
     @foreach( $leads as $lead )
-      <tr>
+      <tr class="shadow" >
         <td class="ls-txt-center"> 
           <a href="/leads/show/{{$lead->id}}">{{ $lead->nome}}</a>
           @if( $lead->matriculado  == true )
           </br><span class="ls-tag-success">Matriculado</span>
+          @endif
+          @if( $lead->open  == false )
+          </br><span class="ls-tag-danger">Novo</span>
           @endif
         </td>
         <td class="ls-txt-center">{{$lead->email}}</td>
@@ -58,10 +83,8 @@ box-shadow: 0px 1px 3px 1px rgba(0,0,0,0.15);">
         <td class="ls-txt-center">
           {{$lead->canal->nome}}
         </td>
-        <td class="ls-txt-center" >{{ $lead->colaborador->name }}</td>
 
         <td class="ls-txt-center ls-regroup">
-        @if( $lead->colaborador_id == \Auth::user()->id )
             <div data-ls-module="dropdown" class="ls-dropdown">
                 <a href="#" class="ls-btn" role="combobox" aria-expanded="false"></a>
                 <ul class="ls-dropdown-nav" aria-hidden="true">
@@ -69,7 +92,6 @@ box-shadow: 0px 1px 3px 1px rgba(0,0,0,0.15);">
                     <li><a href="/leads/delete/{{$lead->id}}" onclick="return confirm('tem certeza que quer excluir?')" class="ls-color-danger" role="option">Excluir</a></li>
                 </ul>
             </div>
-            @endif
         </td>
     
       </tr>
@@ -79,6 +101,77 @@ box-shadow: 0px 1px 3px 1px rgba(0,0,0,0.15);">
   </tbody>
 </table>
 
+{{ $leads->links() }}
+
 </div>
+
+<button data-ls-module="modal" data-target="#modalSmall" class="ls-btn-lg ls-btn-primary ls-ico-user-add"
+ style="-webkit-box-shadow: 0px 2px 4px 1px rgba(0,0,0,0.45);
+-moz-box-shadow: 0px 2px 4px 1px rgba(0,0,0,0.45);
+box-shadow: 0px 2px 4px 1px rgba(0,0,0,0.45);
+font-size:110%;width:60px; height:60px;border-radius:35px; z-index:999;position:fixed;right:50px; bottom:110px;">
+</button>
+
+
+<div class="ls-modal" id="modalSmall">
+<?php $canais = \App\Canal::get(); ?>
+
+  <div class="ls-modal-small">
+    <div class="ls-modal-header">
+      <button data-dismiss="modal">&times;</button>
+      <h4>Novo contato</h4>
+    </div>
+    <div class="ls-modal-body">
+      <form action="/leads/add" class="ls-form" method="post" data-ls-module="form" >
+          @csrf
+          <div class="row">
+              <label class="ls-label col-md-12">
+              <span class="ls-label-text">Nome</span>
+              <input autocomplete="off" type="text" name="nome" required>
+              </label>
+          </div>
+          <div class="row">
+              <label class="ls-label col-md-12">
+              <span class="ls-label-text">Email</span>
+              <input autocomplete="off" type="text" name="email" required>
+              </label>
+          </div>
+
+          <div class="row">
+              <label class="ls-label col-md-12">
+              <span class="ls-label-text">Telefone</span>
+              <input type="text" name="telefone" class="ls-mask-phone9_with_ddd" placeholder="(99) 9999-9999" >
+              </label>
+          </div>
+
+          <div class="row">
+          <label class="ls-label col-md-12 col-sm-12">
+              <b class="ls-label-text">Canal</b>
+              <div class="ls-custom-select">
+                  <select name="canal_id" class="ls-select">
+                  @foreach($canais as $canal)
+                  <option value="{{$canal->id}}"> {{$canal->nome}}</option>
+                  @endforeach
+                  </select>
+              </div>
+          </label>
+          </div>
+          
+          <div class="row">
+              <label class="ls-label col-md-12">
+              <span class="ls-label-text">Observação</span>
+              <textarea rows="2" name="obs" ></textarea>
+              </label>
+          </div>
+
+      
+    </div>
+    <div class="ls-modal-footer">
+          <button type="submit" class="ls-btn-block ls-btn-primary">Adicionar</button>
+    </div>
+    </form>
+  </div>
+</div>
+
 
 @stop
