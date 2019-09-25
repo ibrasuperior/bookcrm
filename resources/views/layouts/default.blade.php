2c62917f5cfdab9e0bbec983c3066210ad7f8276
gustavowebjs <gustavowebjs@gmail.com>
2019-09-25 09:13:12 -0300

    Alterando links locaweb

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -7,15 +7,13 @@
     <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <meta name="description" content="Insira aqui a descrição da página.">
-    <link href="http://assets.locaweb.com.br/locastyle/3.10.1/stylesheets/locastyle.css" rel="stylesheet" type="text/css">
     
+    <link rel="stylesheet" type="text/css" href="//assets.locaweb.com.br/locastyle/edge/stylesheets/locastyle.css">    
     
     <link rel="icon" sizes="192x192" href="/locawebstyle/assets/images/ico-boilerplate.png">
+    
     <link rel="apple-touch-icon" href="/locawebstyle/assets/images/ico-boilerplate.png">
     
-    <!-- JQUERY -->
-    <script src="http://assets.locaweb.com.br/locastyle/3.10.1/javascripts/locastyle.js" type="text/javascript"></script>
-    <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Signika:300,400,600,700&display=swap" rel="stylesheet">
 
 
@@ -209,6 +207,12 @@ $agendas = \App\Agenda::where('colaborador_id', \Auth::user()->id )->get();
     }
     </style>
 
-  
+  <footer>
+    <!-- JQUERY -->
+    <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <!-- Coloque o JS no seu FOOTER, logo depois da jQuery -->
+    <script src="//assets.locaweb.com.br/locastyle/edge/javascripts/locastyle.js"></script>
+
+  </footer>
   </body>
 </html>
