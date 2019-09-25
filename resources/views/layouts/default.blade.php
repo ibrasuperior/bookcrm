107c22cdbb07aef81f8cbc59809d96c587f05bed
gustavowebjs <gustavowebjs@gmail.com>
2019-09-25 09:05:13 -0300

    Alterano link de js locaweb

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,13 +8,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <meta name="description" content="Insira aqui a descrição da página.">
     <link href="http://assets.locaweb.com.br/locastyle/3.10.1/stylesheets/locastyle.css" rel="stylesheet" type="text/css">
+    
+    
     <link rel="icon" sizes="192x192" href="/locawebstyle/assets/images/ico-boilerplate.png">
     <link rel="apple-touch-icon" href="/locawebstyle/assets/images/ico-boilerplate.png">
     
     <!-- JQUERY -->
-
+    <script src="http://assets.locaweb.com.br/locastyle/3.10.1/javascripts/locastyle.js" type="text/javascript"></script>
     <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
-
     <link href="https://fonts.googleapis.com/css?family=Signika:300,400,600,700&display=swap" rel="stylesheet">
 
 
@@ -207,7 +208,6 @@ $agendas = \App\Agenda::where('colaborador_id', \Auth::user()->id )->get();
       color: #666 ;
     }
     </style>
-    <script src="http://assets.locaweb.com.br/locastyle/3.10.1/javascripts/locastyle.js" type="text/javascript"></script>
 
   
   </body>
