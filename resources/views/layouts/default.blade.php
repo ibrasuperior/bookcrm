1eefb17a8a2cc43137e9d5fb50813aa84f8a92e6
Gustavo Vinícius <gustavowebjs@gmail.com>
2020-05-05 11:20:37 -0300

    Update https

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <meta name="description" content="Insira aqui a descrição da página.">
 
-    <link href="http://assets.locaweb.com.br/locastyle/3.10.1/stylesheets/locastyle.css" rel="stylesheet"
+    <link href="https://assets.locaweb.com.br/locastyle/3.10.1/stylesheets/locastyle.css" rel="stylesheet"
         type="text/css">
     <link rel="icon" sizes="192x192" href="/locawebstyle/assets/images/ico-boilerplate.png">
     <link rel="apple-touch-icon" href="/locawebstyle/assets/images/ico-boilerplate.png">
