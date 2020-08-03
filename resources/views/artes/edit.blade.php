e04aa19a69e4a0597a23564b2ee04c46a2b4eb63
galase <galase0408@gmail.com>
2020-08-03 15:44:29 -0300

    resolvendo layout

--- resources/views/artes/edit.blade.php
+++ resources/views/artes/edit.blade.php
@@ -2,39 +2,39 @@
 
 @section('content')
 
+<div class="container-fluid">
+
+    <h1 class="ls-title-intro ls-ico-users">Editar Arte</h1>
 
 
-<h1 class="ls-title-intro ls-ico-users">Editar Arte</h1>
 
+    <form action="/artes/update/{{$artes->id}}" class="ls-form" enctype="multipart/form-data" method="post">
 
+        @csrf
 
-<form action="/artes/update/{{$artes->id}}" class="ls-form" enctype="multipart/form-data" method="post">
+        <legend class="ls-title-2">Editar Arte</legend>
+        <div class="row">
+            <label class="ls-label col-md-6">
+                <span class="ls-label-text">Nome</span>
+                <input autocomplete="off" type="text" name="nome" value="{{ $artes->nome  }}" required>
+            </label>
+        </div>
+        <div class="row">
+            <label class="ls-label col-md-6">
+                <b class="ls-label-text">Editavel</b>
+                <div class="ls-custom-select ls-field-sm">
+                    <select name="personalizavel" class="ls-select">
+                        <option value="1" @if($artes->personalizavel == 1) selected="selected" @endif > Sim </option>
+                        <option value="0" @if($artes->personalizavel == 0) selected="selected" @endif> Não </option>
+                    </select>
+                </div>
+            </label>
+        </div>
 
-    @csrf
-
-    <legend class="ls-title-2">Editar Arte</legend>
-    <div class="row">
-        <label class="ls-label col-md-6">
-            <span class="ls-label-text">Nome</span>
-            <input autocomplete="off" type="text" name="nome" value="{{ $artes->nome  }}" required>
-        </label>
-    </div>
-    <div class="row">
-        <label class="ls-label col-md-6">
-            <b class="ls-label-text">Editavel</b>
-            <div class="ls-custom-select ls-field-sm">
-                <select name="personalizavel" class="ls-select">
-                    <option value="1" @if($artes->personalizavel == 1) selected="selected" @endif > Sim </option>
-                    <option value="0" @if($artes->personalizavel == 0) selected="selected" @endif> Não </option>
-                </select>
-            </div>
-        </label>
-    </div>
-
-    <button type="submit" class="ls-btn-primary">Alterar</button>
-    <a href="/artes" class="ls-btn">Voltar</a>
-</form>
-
+        <button type="submit" class="ls-btn-primary">Alterar</button>
+        <a href="/artes" class="ls-btn">Voltar</a>
+    </form>
+</div>
 
 
 
